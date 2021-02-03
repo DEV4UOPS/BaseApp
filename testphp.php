ad9128e6076cab6ff94f85237a22afb5228e36af
bfree999 <bm.tempo999@gmail.com>
2021-02-03 01:41:12 +0100

    roots

---
+++ testphp.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test de PHP</title>
+</head>
+<body>
+    <h1>Test de Php</h1>
+    <p>
+        <?php
+            echo "Si vous voyez ceci c'est que PHP fonctionne.";
+        ?>
+    </p>
+    <p>
+        S'il n'y a rien entre le titre et ce texte, c'est que php ne fonctionne pas.
+    </p>
+</body>
+</html>
